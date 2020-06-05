582569b9c3167ff40f4989ba8625cafea4f9e264
tomoyaueno29 <u.t012807@gmail.com>
2020-06-05 19:18:16 +0900

    Update store.go

--- ch06/sqlx/store.go
+++ ch06/sqlx/store.go
@@ -20,5 +20,15 @@ func init() {
 
 func GetPost(id int) (post Post, err error) {
 	post = Post{}
-	err =
+	err = Db.QueryRowx("select id, content, author from posts where id=$1", id).StructScan(&post)
+	if err != nil {
+		return
+	}
+	return
 }
+
+func (post *Post) Create() (err error) {
+	err = Db.QueryRow("insert into posts (content, author) values ($1, $2) returning id", post.Content, post.AuthorName).Scan(&post.Id)
+	return
+}
+
