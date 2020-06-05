5539877dbf9ca720dd3e93dddfe1d59fb22131b7
tomoyaueno29 <u.t012807@gmail.com>
2020-06-05 17:01:10 +0900

    Update crud_prac.go

--- crud_prac.go
+++ crud_prac.go
@@ -52,6 +52,22 @@ func (post  *Post) delete() (err error) {
 	return
 }
 
+func posts(limit int) (posts []Post, err error) {
+	rows, err := Db.Query("select id, content, author from posts limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil{
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
 
 func main() {
 	post := Post{Content: "Hello!", Author: "Sau"}
