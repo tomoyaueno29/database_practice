0eb8da5596287d761650adaf27b0ccc8d99b10f5
tomoyaueno29 <u.t012807@gmail.com>
2020-06-05 16:02:27 +0900

    Update crud_prac.go

--- crud_prac.go
+++ crud_prac.go
@@ -1,1 +1,19 @@
 package main
+
+import "database/sql"
+
+type Post struct{
+	Id int
+	Content string
+	Author string
+}
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=tomoyaueno dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
