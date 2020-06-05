e28cf0ddc41d0ffe2a27dfc945275e4a665ad390
tomoyaueno29 <u.t012807@gmail.com>
2020-06-05 18:36:07 +0900

    Update sql.go

--- ch06/sql.go
+++ ch06/sql.go
@@ -1,1 +1,47 @@
-package ch06
+package main
+
+import "database/sql"
+
+type Post struct {
+	Id int
+	Content string
+	Author string
+}
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=tomoyaueno dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Posts(limit int) (posts []Post, err error){
+
+	rows, err := Db.Query("select id, content, author from posts limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next(){
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+func GetPost(id int) (post Post, err error){
+	post = Post{}
+	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+func main() {
+
+}
