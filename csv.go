c3470e289f267fa83facddfb23766b1762fe0e93
tomoyaueno29 <u.t012807@gmail.com>
2020-06-05 14:12:42 +0900

    Update csv.go

--- csv.go
+++ csv.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+)
+
+type Post struct{
+	Id int
+	Content string
+	Author string
+}
+
+func main() {
+	csvFile, err := os.Create("posts.csv")
+	if err !=  nil{
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	allPosts := []Post{
+		Post{Id:1, Content:"Hello!", Author:"Sau Sheong"},
+		Post{Id:2, Content:"World!", Author:"Pedro"},
+	}
+
+	writer := csv.NewWriter(csvFile)
+	for _, post := range allPosts{
+		line := []string{strconv.Itoa(post.Id), post.Content, post.Author}
+
+		err := writer.Write(line)
+		if 
+	}
+}
