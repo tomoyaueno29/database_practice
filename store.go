77b6cea9bfac54a6ab1977a65540cfe1c8acb892
tomoyaueno29 <u.t012807@gmail.com>
2020-06-05 17:32:54 +0900

    Update store.go

--- store.go
+++ store.go
@@ -40,3 +40,25 @@ func (comment *Comment) Create() (err error){
 	comment.Post.Id).Scan(&comment.Id)
 	return
 }
+
+func GetPost(id int) (post Post, err error){
+	post = Post{}
+	post.Comments = []Comment{}
+	err = Db.QueryRow(`select id, content, author from posts 
+	where id = $1`, id).Scan(&post.Id, &post.Content, &post.Author)
+
+	rows, err := Db.Query("select id, content, author from comments")
+	if err != nil {
+		return
+	}
+	for rows.Next(){
+		comment := Comment{Post: &post}
+		err = rows.Scan(&comment.Id, &comment.Content, &comment.Author)
+		if err != nil {
+			return
+		}
+		post.Comments = append(post.Comments, comment)
+	}
+	rows.Close()
+	return
+}
