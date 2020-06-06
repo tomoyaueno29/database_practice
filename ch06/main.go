cc3e1c91003b88c8682e046685257bc67eda8974
tomoyaueno29 <u.t012807@gmail.com>
2020-06-06 20:58:26 +0900

    Update main.go

--- ch06/main.go
+++ ch06/main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
 func main() {
 
-		rand.Seed(time.Now().UnixNano())
-			dise := rand.Intn(6)
-			fmt.Println(dise)
-	switch dise + 1 {
-
-	}
-			if dise==6{
-				fmt.Println("大吉")
-			}else if dise==5 || dise==4{
-				fmt.Println("中吉")
-			}else if dise==3 || dise==2{
-				fmt.Println("吉")
-			}else{
-				fmt.Println("凶")
-			}
 }
